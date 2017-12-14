a7a64a0d08ae0fe436c0c592ceaa12205a1f7db7
Diana Davis <dianadryerdavis@gmail.com>
2017-12-13 21:02:27 -0500

    Added function to hide button, show pic.

--- myjsfile.js
+++ myjsfile.js
@@ -1,45 +1,6 @@
-in html
-element.addEventListener('event_name_here', eventHandlerFunctionHere)
-
-
-
-
 function showThePic() {
-    show('spoiler-box') ;
+    const myElement = document.getElementById('spoiler-box');
+    const myButton = document.getElementById('my-button');
+        myElement.style.display = "block";
+        myButton.style.display = "none";
 }
-
-
-
-// const is a keyword used to signify a variable that wont change (that // name cant be reassigned to another value), it is shorthand for "constant"
-// const chosenElement = document.getElementById('chosenElementID');
-
-// add the event listener for a "click" event
-//chosenElement.addEventListener('click', clickHandlerFunction)
-
-
-//function clickHandlerFunction() {
-//    chosenElement. // here is where you would call some DOM manipulating method which could enlarge the element, change its color, whatever you want to do
-//}
-
-
-function on() {
-    document.getElementById("spoiler-box").style.display = "block";
-}
-
-function off() {
-    document.getElementById("spoiler-box").style.display = "none";
-}
-
-
-
-
-w
-
-
-  <script src="myjsfile.js"></script>
-
-  function popupAnAlert() {
-      alert("hello")
-  }
-
-  <button onclick="popupAnAlert()">Click to see alert</button>
